ee66171ef6d977925a01bee6bbe061ec1872a5d0
Gregory Werbin <gwerbin@users.noreply.github.com>
2018-11-19 22:15:44 -0500

    Use setuptools, add interim maintainer info

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
 setup(
     name = 'hymagic',
-    packages = ['hymagic'], # this must be the same as the name above
-    version = '0.1',
+    packages = find_packages(),
+    version = '0.1.1',
     description = 'IPython magic for hylang',
     author = 'Todd Iverson',
     author_email = 'tiverson@smumn.edu',
-    url = 'https://github.com/yardsale8/hymagic',   # use the URL to the github repo
-    download_url = 'https://github.com/yardsale8/hymagic/tarball/0.1', # I'll explain this in a second
+    maintainer = 'Greg Werbin',
+    maintainer_email = 'outthere@me.gregwerbin.com',
+    url = 'https://github.com/gwerbin/hymagic',   # use the URL to the github repo
+    download_url = 'https://github.com/gwerbin/hymagic/archive/0.1.1.tar.gz', # I'll explain this in a second
     keywords = ['hylang', 'IPython extension', 'IPython magic'], # arbitrary keywords
-    classifiers = ["Development Status :: 3 - Alpha"],
+    classifiers = ["Development Status :: 3 - Alpha"]
 )
